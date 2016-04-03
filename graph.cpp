ae1e5fad996362de239acde9059cf0896d02ea9c
Giovanni Paolo <giovanni1995@gmail.com>
2016-04-03 05:27:38 -0300

    Add edge_adder helper function and dependencies

--- graph.cpp
+++ graph.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 #include "graph.h"
 
 using namespace std;
+
+void edge_adder(Graph & g, int v, string line){
+	char * cp;
+	cp = strtok(strdup(line.c_str()), " ");
+	while (cp != nullptr){
+		g.addEdge(v, atoi(cp));
+		cp = strtok(nullptr, " ");
+	}
+}
 
 Graph::Graph(int n) {
 	vertices = n;
