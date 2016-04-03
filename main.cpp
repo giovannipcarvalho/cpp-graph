349a56add912615f3c3695dcd814900122c1e1e4
Giovanni Paolo <giovanni1995@gmail.com>
2016-04-03 05:53:15 -0300

    Read arguments in main

--- main.cpp
+++ main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	if (argc > 1){
+		Graph g(argv[1]);
+		cout << "Checking edge 0->0: " << g.edgeExists(0, 0) << endl;
+		cout << "Checking edge 0->1: " << g.edgeExists(0, 1) << endl;
+	}
 	return 0;
 }
