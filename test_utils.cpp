1d43d877ab6baee25739f307ece26f40a37e7bd3
pandoxer <giovanni1995@gmail.com>
2016-04-18 20:41:46 +0000

    add test case for hasLoop

--- test_utils.cpp
+++ test_utils.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 int main(){
     Graph g(2);
+    g.addEdge(0, 0);
 	g.addEdge(0, 1);
 	g.addEdge(1, 0);
 	
     cout << isSymmetric(g) << endl;
+    cout << hasLoop(g) << endl;
 }
