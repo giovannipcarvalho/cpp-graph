a74b6d70d0a9b1b229efceeffd750da51a44c2d0
Giovanni Paolo <giovanni1995@gmail.com>
2016-04-27 17:42:01 -0300

    Add isCyclic method

--- utils.h
+++ utils.h
@@ -21,4 +21,31 @@ bool hasLoop(const Graph g){
     return false;
 }
 
+enum Color { white, gray, black };
+
+bool DFS(const Graph & g, int u, vector<Color> & c){
+    c[u] = gray;
+    for(auto v : g.successors(u)){
+        if(c[v] == white){
+            return DFS(g, v, c);
+        } else if(c[v] == gray){
+            return true;
+        }
+    }
+    c[u] = black;
+    return false;
+}
+
+bool isCyclic(const Graph g){
+    vector<Color> c = vector<Color>(g.getVerticeCount(), white);
+    for (int i = 0; i < g.getVerticeCount(); ++i){
+        if(c[i] == white){
+            bool hasCycle = DFS(g, i, c);
+            if (hasCycle)
+                return true;
+        }
+    }
+    return false;
+}
+
 #endif
